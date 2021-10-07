09aed12b61ee3ff2d4bb7d4aa7830b8c52a920c5
JeisonHuertas <jeisonhuertas99@gmail.com>
2021-10-06 20:23:11 -0500

    defini metodos y atributos en la clase LineaCelular

--- src/com/mundo/LineaCelular.java
+++ src/com/mundo/LineaCelular.java
@@ -1,5 +1,38 @@
 package com.mundo;
 
-public class LineaCelular {
-
+public class LineaCelular extends LineaTelefonica{
+	private int numeroMinutosLocal;
+	private int numeroMinutosCelular;
+	private double saldoDisponibleLocal;
+	
+	public LineaCelular() {
+		this.numeroMinutosLocal = 0;
+		this.numeroMinutosCelular = 0;
+		this.saldoDisponibleLocal = 50000;
+		this.numeroLlamadas = 0;
+		this.numeroMinutos = 0;
+	}
+	public int darNumeroMinutosLocal() {
+		return numeroMinutosLocal;
+	}
+	public int darNumeroMinutosCelular() {
+		return numeroMinutosCelular;
+	}
+	public double darSaldoDisponibleLocal() {
+		return saldoDisponibleLocal;
+	}
+	public double calcularCostoLlamadasLocal() {
+		return   numeroMinutosLocal* 20;
+	}
+	public void agregarLlamadaLocal(int minutos) {
+		this.numeroMinutosLocal += minutos;
+		this.numeroMinutos += minutos;
+		this.numeroLlamadas += 1;
+	}
+	public void agregarLlamadaCelular(int minutos) {
+		this.numeroMinutosCelular += minutos;
+		this.numeroMinutos += minutos;
+		this.numeroLlamadas += 1;
+	}
+	
 }
