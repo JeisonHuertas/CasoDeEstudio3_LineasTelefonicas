90bb201c7ef15bf9b89f53f068177746d53d4d63
JeisonHuertas <jeisonhuertas99@gmail.com>
2021-10-09 17:17:22 -0500

    funcionalidad del panel1 realizada

--- src/com/interfaz/Interfaz.java
+++ src/com/interfaz/Interfaz.java
@@ -3,13 +3,23 @@ package com.interfaz;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+
+import com.mundo.Empresa;
+
+import java.awt.GridLayout;
 
 public class Interfaz extends JFrame {
 
 	private JPanel contentPane;
+	// asociaciones con paneles lineas 1,2 y3
+	private PanelLinea1 panelLinea1;
+	private Empresa empresa;
+	
 
 	/**
 	 * Launch the application.
@@ -31,12 +41,62 @@ public class Interfaz extends JFrame {
 	 * Create the frame.
 	 */
 	public Interfaz() {
+		empresa = new Empresa();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1116, 512);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(2, 3, 5, 5));
+		
+		panelLinea1 = new PanelLinea1(this);
+		contentPane.add(panelLinea1);
 	}
-
+	
+	public void agregarLlamada(int numeroLinea, JTextField txtMinutosLlamadas, JCheckBox chckLocal, JCheckBox chckLargaDistancia,  JCheckBox chckCelular) {
+		int minutos = Integer.parseInt(txtMinutosLlamadas.getText());
+		
+		if (!chckLocal.isSelected() && !chckLargaDistancia.isSelected() && !chckCelular.isSelected()) {
+			return;
+		}
+		else if (chckLocal.isSelected() && !chckLargaDistancia.isSelected() && !chckCelular.isSelected()) {
+			if (numeroLinea == 1) {
+				empresa.agregarLlamadaLocalLinea1(minutos);
+			}else if (numeroLinea == 2) {
+				empresa.agregarLlamadaLocalLinea2(minutos);
+			}else if (numeroLinea == 3) {
+				empresa.agregarLlamadaLocalLinea3(minutos);
+			}
+		}
+		else if (!chckLocal.isSelected() && chckLargaDistancia.isSelected() && !chckCelular.isSelected()) {
+			if (numeroLinea == 1) {
+				empresa.agregarLlamadaLargaDistanciaLinea1(minutos);
+			}else if (numeroLinea == 2) {
+				empresa.agregarLlamadaLargaDistanciaLinea2(minutos);
+			}else if (numeroLinea == 3) {
+				empresa.agregarLlamadaLargaDistanciaLinea3(minutos);
+			}
+		}
+		else if (!chckLocal.isSelected() && !chckLargaDistancia.isSelected() && chckCelular.isSelected()) {
+			if (numeroLinea == 1) {
+				empresa.agregarLlamadaCelularLinea1(minutos);
+			}else if (numeroLinea == 2) {
+				empresa.agregarLlamadaCelularLinea2(minutos);
+			}else if (numeroLinea == 3) {
+				empresa.agregarLlamadaCelularLinea3(minutos);
+			}
+		}
+		else {
+			chckLocal.setSelected(false);
+			chckLargaDistancia.setSelected(false);
+			chckCelular.setSelected(false);
+		}
+	}
+	public void actualizar( JTextField txtMinutosLlamadas, JCheckBox chckLocal, JCheckBox chckLargaDistancia,  JCheckBox chckCelular) {
+		panelLinea1.actualizar(empresa.darLinea1());
+		txtMinutosLlamadas.setText("");
+		chckLocal.setSelected(false);
+		chckLargaDistancia.setSelected(false);
+		chckCelular.setSelected(false);
+	}
 }
--- src/com/interfaz/PanelLinea1.java
+++ src/com/interfaz/PanelLinea1.java
@@ -1,14 +1,134 @@
 package com.interfaz;
 
 import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.border.TitledBorder;
+
+import com.mundo.LineaTelefonica;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PanelLinea1 extends JPanel {
+	private JTextField txtValorTotalLinea;
+	private JTextField txtTotalLlamadas;
+	private JTextField txtTotalMinutos;
+	private JTextField txtMinutosLlamadas;
+	private JCheckBox chckLocal;
+	private JCheckBox chckLargaDistancia;
+	private JCheckBox chckCelular;
+	// Asociacion con la clase Empresa
+	private Interfaz principal;
+	
+	public PanelLinea1(Interfaz principal) {
+		this();
+		this.principal = principal;
+	}
 
 	/**
 	 * Create the panel.
 	 */
 	public PanelLinea1() {
+		setBorder(new TitledBorder(null, "Linea 1", TitledBorder.CENTER, TitledBorder.ABOVE_TOP, null, Color.ORANGE));
+		setLayout(new GridLayout(6, 3, 5, 2));
+		
+		JLabel lblNewLabel = new JLabel("");
+		add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Valor Total Linea");
+		lblNewLabel_1.setForeground(Color.ORANGE);
+		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 12));
+		add(lblNewLabel_1);
+		
+		txtValorTotalLinea = new JTextField();
+		txtValorTotalLinea.setEditable(false);
+		txtValorTotalLinea.setColumns(10);
+		add(txtValorTotalLinea);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Total Llamadas");
+		lblNewLabel_1_1.setForeground(Color.ORANGE);
+		lblNewLabel_1_1.setFont(new Font("Arial", Font.BOLD, 12));
+		add(lblNewLabel_1_1);
+		
+		txtTotalLlamadas = new JTextField();
+		txtTotalLlamadas.setEditable(false);
+		txtTotalLlamadas.setColumns(10);
+		add(txtTotalLlamadas);
+		
+		JLabel lblNewLabel_3 = new JLabel("");
+		add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Total Minutos");
+		lblNewLabel_1_2.setForeground(Color.ORANGE);
+		lblNewLabel_1_2.setFont(new Font("Arial", Font.BOLD, 12));
+		add(lblNewLabel_1_2);
+		
+		txtTotalMinutos = new JTextField();
+		txtTotalMinutos.setEditable(false);
+		txtTotalMinutos.setColumns(10);
+		add(txtTotalMinutos);
+		
+		JLabel lblNewLabel_4 = new JLabel("Minutos Llamada");
+		lblNewLabel_4.setForeground(Color.ORANGE);
+		lblNewLabel_4.setFont(new Font("Arial", Font.BOLD, 12));
+		add(lblNewLabel_4);
+		
+		txtMinutosLlamadas = new JTextField();
+		txtMinutosLlamadas.setColumns(10);
+		add(txtMinutosLlamadas);
+		
+		JLabel lblNewLabel_5 = new JLabel("");
+		add(lblNewLabel_5);
+		
+		chckLocal = new JCheckBox("Local");
+		chckLocal.setFont(new Font("Arial", Font.BOLD, 12));
+		add(chckLocal);
+		
+		chckLargaDistancia = new JCheckBox("Larga Distancia");
+		chckLargaDistancia.setFont(new Font("Arial", Font.BOLD, 12));
+		add(chckLargaDistancia);
+		
+		chckCelular = new JCheckBox("Celular");
+		chckCelular.setFont(new Font("Arial", Font.BOLD, 12));
+		add(chckCelular);
+		
+		JLabel lblNewLabel_6 = new JLabel("");
+		add(lblNewLabel_6);
+		
+		JButton btnAgregarLlamada = new JButton("Agregar Llamada");
+		btnAgregarLlamada.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				principal.agregarLlamada(1, txtMinutosLlamadas, chckLocal, chckLargaDistancia, chckCelular);
+				principal.actualizar(txtMinutosLlamadas, chckLocal, chckLargaDistancia, chckCelular);
+				
+			}
+		});
+		btnAgregarLlamada.setBackground(Color.ORANGE);
+		btnAgregarLlamada.setFont(new Font("Arial", Font.BOLD, 12));
+		add(btnAgregarLlamada);
+		
+		JLabel lblNewLabel_7 = new JLabel("");
+		add(lblNewLabel_7);
 
+	}
+	public void actualizar(LineaTelefonica linea) {
+		txtValorTotalLinea.setText(formatearValor(linea.darCostoLlamadas()));
+		txtTotalLlamadas.setText(formatearValorEntero(linea.darNumeroLlamadas()));
+		txtTotalMinutos .setText(formatearValorEntero(linea.darNumeroMinutos()));
+	}
+	private String formatearValor(double valor) {
+		return Double.toString(valor);
+	}
+	private String formatearValorEntero(int valor) {
+		return Integer.toString(valor);
 	}
 
 }
